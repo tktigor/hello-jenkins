38890397308804e65feaccc724e7ab34f9e853ac
igor <root@outils.int.globepayroll.com>
2015-06-16 14:49:01 +0000

    marche putain

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ var app = require('../app.js');
  
 describe('GET /', function() {
   it('respond with hello world', function(done) {
-    request(app).get('/').expect('hello Andeol', done);
+    request(app).get('/').expect('hello Igor', done);
   });
 });
